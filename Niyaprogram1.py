d6bd568223efb2641910b61832c774df87864197
niyapius2607333 <niyapius2028@cs.sjcetpalai.ac.in>
2025-01-29 13:10:53 +0530

    niyaprogram1.py

---
+++ Niyaprogram1.py
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+ int main(){
+
+ int age;
+
+ printf("\nEnter the age of the person");
+
+ scanf("%d",&age);
+
+ if(age>=18){
+
+ printf("Congratulation! You are eligible for casting your own vote");
+
+ }
+
+ else{
+
+ printf("Sorry You are not eligible for casting your own vote");
+
+ }
+
+ return 0;
+
+}
