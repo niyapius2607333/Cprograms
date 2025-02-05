435e3a5f8259460a52bd1a891d0d9d982d9eac73
niyapius2607333 <niyapius2028@cs.sjcetpalai.ac.in>
2025-02-05 15:46:35 +0530

    Add files via upload

---
+++ sequentialsearch.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+  int main(){
+      int i,number,limit;
+      printf("Enter the limit of array");
+      scanf("%d",&limit);
+      int num[limit];
+      for(i=0;i<limit;i++)
+      {
+      	printf("Enter the elements");
+      	scanf("%d",&num[i]);
+      }
+      printf("Enter the number to be searched");
+      scanf("%d",&number);
+      for(i=0;i<limit;i++)
+      {
+      	if(num[i]==number){
+        	printf("The number is found ");
+        	printf("%d",i+1);
+        }
+      }
+     return 0;
+ }
