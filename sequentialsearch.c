d9ab729871a07fa123d82d3ec70cc85a7e3b82f4
niyapius2607333 <niyapius2028@cs.sjcetpalai.ac.in>
2025-02-07 10:28:56 +0530

    sequentialsearch.c

--- sequentialsearch.c
+++ sequentialsearch.c
@@ -1,7 +1,7 @@
 '''
 Author:NIYA PIUS
 Date : 05-02-2025
-C program to find whether a person is eligible for voting or not
+C program to do sequential search in an array
 '''
 #include<stdio.h>
   int main(){
